337757f9ff655202417155d4b8c4ff7a2816ef38
olisegun <eolisegun83@gmail.com>
2020-06-02 07:27:58 +0100

    better

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        address = data["address"]
+        num = data["num"]
+        state = data["state"]
+        country = data["country"]
+        csv_writer = csv.writer(database, delimiter =',', quotechar ='"', quoting = csv.QUOTE_MINIMAL )
+        csv_writer.writerow([name,email,address,num,state,country])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		data = request.form.to_dict()
+		write_to_csv(data)
+		return redirect('/thanks.html')
+	else:
+		return 'try again'
